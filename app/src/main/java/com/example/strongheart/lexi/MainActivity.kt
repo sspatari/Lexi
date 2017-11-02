7b29ccc695b48b5e64e6f033b3969de2aa1915ca
sspatari <spatari.stanislav@gmail.com>
2017-11-02 05:27:31 +0100

    Change the first screen recycleView to contain all the element on the screen.

--- app/src/main/java/com/example/strongheart/lexi/MainActivity.kt
+++ app/src/main/java/com/example/strongheart/lexi/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.strongheart.lexi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        my_recycler_view.layoutManager = LinearLayoutManager(this)
+        my_recycler_view.setHasFixedSize(true)
+        val myDataset= Array(20, {x -> x.toString()})
+        my_recycler_view.adapter = MyAdapter(myDataset)
+
     }
 }
-    
